92c2841a7a3baf043c734f8f2013d1691bc32882
Emmanuel Elegbede <emmanuelhemarxyll@gmail.com>
2021-01-14 20:08:23 +0000

    little changes

--- src/Filters/Filters.js
+++ src/Filters/Filters.js
@@ -5,17 +5,17 @@ import FilterBar from './FilterBar/FilterBar'
 
 const searchBar = (props) => {
   return (
-    <Col  className={classes.Filter}>
-   <Input 
-   className={classes.Search} 
-   type='text' 
-   value={props.search} 
-   onChange={props.changed} 
-   placeholder="Search Name" />
-<div className={classes.Select}>
-    <FilterBar filter={props.filter} /></div>
- </Col>
-    );
+    <Col className={classes.Filter}>
+      <Input
+        className={classes.Search}
+        type='text'
+        value={props.search}
+        onChange={props.changed}
+        placeholder="Search Name" />
+      <div className={classes.Select}>
+        <FilterBar filter={props.filter} /></div>
+    </Col>
+  );
 };
 
 export default searchBar;
--- src/Pagination/Pagination.js
+++ src/Pagination/Pagination.js
@@ -2,21 +2,21 @@ import React from 'react';
 import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
 
 const pagination = (props) => {
- const pageNumbers = [];
+  const pageNumbers = [];
 
- for (let i = 1; i<= Math.ceil(props.totalProfiles / props.profilesPerPage); i++){
-   pageNumbers.push(i)
- }
- return (
-  <Pagination aria-label='Page '>
+  for (let i = 1; i <= Math.ceil(props.totalProfiles / props.profilesPerPage); i++) {
+    pageNumbers.push(i)
+  }
+  return (
+    <Pagination aria-label='Page '>
 
-    {pageNumbers.map(number => (
-      <PaginationItem active={number === props.currentPage}>
-      <PaginationLink  key={number} onClick={()=> props.pageLink(number)}>{number}</PaginationLink>
-      </PaginationItem>
-    ))}
-  </Pagination>
- )
+      {pageNumbers.map(number => (
+        <PaginationItem active={number === props.currentPage}>
+          <PaginationLink key={number} onClick={() => props.pageLink(number)}>{number}</PaginationLink>
+        </PaginationItem>
+      ))}
+    </Pagination>
+  )
 };
 
 export default pagination;
